7184f804d488e4abc112d5864e0df06fb1e5035d
ilyasvintsov <zaz1993@gmail.com>
2024-10-06 23:01:59 +0200

    time limit

--- src/main/java/com/svintsov/cinema/Solution.java
+++ src/main/java/com/svintsov/cinema/Solution.java
@@ -1,28 +1,75 @@
 package com.svintsov.cinema;
 
-import lombok.extern.slf4j.Slf4j;
+import static java.util.Map.entry;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-@Slf4j
 class Solution {
 
-    public List<List<Integer>> combine(List<Integer> rowOccupation, Set<Entry<Integer, Integer>> aisles, int familySize) {
+    private static final int ROW_LENGTH = 10;
+    private static final int FAMILY_SIZE = 4;
+    private static final Set<Entry<Integer, Integer>> AISLES = Set.of(entry(2, 3), entry(6, 7));
+
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+
+        Map<Integer, List<Integer>> rowNumbersToOccupationFlags = Arrays.stream(reservedSeats)
+                .collect(Collectors.groupingBy(ints -> ints[0], mapping(ints -> ints[1], toList())))
+                .entrySet()
+                .stream()
+                .map(rowNumberToOccupiedSeatsNumbers -> {
+                    List<Integer> occupationFlags = IntStream.range(1, ROW_LENGTH + 1)
+                            .mapToObj(seatNumber -> rowNumberToOccupiedSeatsNumbers.getValue().contains(seatNumber) ? 1:0)
+                            .toList();
+                    return entry(rowNumberToOccupiedSeatsNumbers.getKey(), occupationFlags);
+                })
+                .collect(toMap(Entry::getKey, Entry::getValue));
+
+        for (int i = 1; i <= n; ++i) {
+            if (rowNumbersToOccupationFlags.containsKey(i)) {
+                continue;
+            } else {
+                rowNumbersToOccupationFlags.put(i, emptyListOfSize(ROW_LENGTH));
+            }
+        }
+
+        return rowNumbersToOccupationFlags
+                .values()
+                .stream()
+                .map(occupationFlags -> {
+                    int maxSegmentsCount = combine(occupationFlags, AISLES, FAMILY_SIZE).getMaxSegmentsCount();
+//                    System.out.println(occupationFlags);
+//                    System.out.println(maxSegmentsCount);
+                    return maxSegmentsCount;
+                })
+                .reduce(Integer::sum)
+                .orElse(0);
+    }
+
+    public CombineResponse combine(List<Integer> rowOccupation, Set<Entry<Integer, Integer>> aisles, int familySize) {
 
         ArrayList<List<Integer>> possibleCombinations = new ArrayList<>();
 
-        List<Integer> emptyRow = new ArrayList<>(Stream.generate(() -> 0)
-                .limit(rowOccupation.size())
-                .toList());
+        List<Integer> emptyRow = emptyListOfSize(rowOccupation.size());
 
-        combineInternal(rowOccupation, emptyRow, aisles, familySize, 0, possibleCombinations);
+        AtomicInteger currentSegmentsCount = new AtomicInteger(0);
+        AtomicInteger maxSegmentsCount = new AtomicInteger(0);
 
-        return possibleCombinations;
+        combineInternal(rowOccupation, emptyRow, aisles, familySize, 0,
+                maxSegmentsCount, currentSegmentsCount, possibleCombinations);
+
+        return new CombineResponse(possibleCombinations, maxSegmentsCount.get());
     }
 
     private void combineInternal(
@@ -31,32 +78,48 @@ class Solution {
             Set<Entry<Integer, Integer>> aisles,
             int segmentSize,
             int lastPosition,
+            AtomicInteger maxSegmentsCount,
+            AtomicInteger currentSegmentsCount,
             List<List<Integer>> possibleCombinations) {
 
         int currentStartPosition;
         if (lastPosition == 0) {
             currentStartPosition = 0;
         } else {
-            currentStartPosition = lastPosition + 2;
+            currentStartPosition = lastPosition + 1;
         }
 
         if ((currentCombination.size() - currentStartPosition) < segmentSize) {
             possibleCombinations.add(new ArrayList<>(currentCombination));
+            if (currentSegmentsCount.get() > maxSegmentsCount.get()) {
+                maxSegmentsCount.set(currentSegmentsCount.get());
+            }
             return;
         }
 
+        boolean combinationIsFinal = true;
         while (currentStartPosition < currentCombination.size() - 1) {
             if (possibleToPutSegment(segmentSize, currentOccupation, aisles, currentCombination, currentStartPosition)) {
+                combinationIsFinal = false;
                 for (int j = 0; j < segmentSize; ++j) {
                     currentCombination.set(currentStartPosition + j, 1);
                 }
+                currentSegmentsCount.incrementAndGet();
                 combineInternal(currentOccupation, currentCombination, aisles, segmentSize, currentStartPosition + segmentSize - 1,
-                        possibleCombinations);
+                        maxSegmentsCount, currentSegmentsCount, possibleCombinations);
+                currentSegmentsCount.decrementAndGet();
                 for (int j = 0; j < segmentSize; ++j) {
                     currentCombination.set(currentStartPosition + j, 0);
                 }
             }
             currentStartPosition++;
+        }
+
+        if (combinationIsFinal) {
+            possibleCombinations.add(new ArrayList<>(currentCombination));
+            if (currentSegmentsCount.get() > maxSegmentsCount.get()) {
+                maxSegmentsCount.set(currentSegmentsCount.get());
+            }
         }
     }
 
@@ -73,17 +136,42 @@ class Solution {
                 return false;
             }
             if (i == 0) {
-                if (aisles.contains(Map.entry(startPosition, startPosition + 1))) {
+                if (aisles.contains(entry(startPosition, startPosition + 1))) {
                     return false;
                 }
             }
             if (i == segmentSize - 1) {
-                if (aisles.contains(Map.entry(startPosition + segmentSize, startPosition + segmentSize - 1))) {
+                if (aisles.contains(entry(startPosition + segmentSize - 2, startPosition + segmentSize - 1))) {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    private List<Integer> emptyListOfSize(int size) {
+        return new ArrayList<>(Stream.generate(() -> 0)
+                .limit(size)
+                .toList());
+    }
+
+    public static class CombineResponse {
+
+        private final List<List<Integer>> allCombinations;
+        private final int maxSegmentsCount;
+
+        public CombineResponse(List<List<Integer>> allCombinations, int maxSegmentsCount) {
+            this.allCombinations = allCombinations;
+            this.maxSegmentsCount = maxSegmentsCount;
+        }
+
+        public List<List<Integer>> getAllCombinations() {
+            return allCombinations;
+        }
+
+        public int getMaxSegmentsCount() {
+            return maxSegmentsCount;
+        }
     }
 
 }
--- src/test/java/com/svintsov/cinema/SolutionTest.java
+++ src/test/java/com/svintsov/cinema/SolutionTest.java
@@ -4,6 +4,7 @@ import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Sets.newHashSet;
 import static java.util.Map.entry;
 
+import com.svintsov.cinema.Solution.CombineResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,14 +24,54 @@ class SolutionTest {
     }
 
     @Test
-    public void test() {
+    public void testWithoutOccupation() {
         List<Integer> occupation = newArrayList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-        Set<Entry<Integer, Integer>> aisles = newHashSet(entry(2,3), entry(6,7));
+        Set<Entry<Integer, Integer>> aisles = newHashSet(entry(2, 3), entry(6, 7));
 
-        List<List<Integer>> result = solution.combine(occupation, aisles, 4);
+        CombineResponse result = solution.combine(occupation, aisles, 4);
 
-        result.forEach(integers -> log.info("{}", integers));
+        result.getAllCombinations().forEach(integers -> log.info("{}", integers));
+        log.info("max families: {}", result.getMaxSegmentsCount());
+    }
+
+    @Test
+    public void testWithOccupation() {
+        List<Integer> occupation = newArrayList(1, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+
+        Set<Entry<Integer, Integer>> aisles = newHashSet(entry(2, 3), entry(6, 7));
+
+        CombineResponse result = solution.combine(occupation, aisles, 4);
+
+        result.getAllCombinations().forEach(integers -> log.info("{}", integers));
+        log.info("max families: {}", result.getMaxSegmentsCount());
+    }
+
+    @Test
+    public void testSolution() {
+
+//        [[4,7],[4,1],[3,1],[5,9],[4,4],[3,7],[1,3],[5,5],[1,6],[1,8],[3,9],[2,9],[1,4],[1,9],[1,10]]
+
+        int maxFamilies = solution.maxNumberOfFamilies(5, new int[][]{
+                new int[]{4, 7},
+                new int[]{4, 1},
+                new int[]{3, 1},
+                new int[]{5, 9},
+                new int[]{4, 4},
+                new int[]{3, 7},
+                new int[]{1, 3},
+                new int[]{5, 5},
+                new int[]{1, 6},
+                new int[]{1, 8},
+                new int[]{3, 9},
+                new int[]{2, 9},
+                new int[]{1, 4},
+                new int[]{1, 9},
+                new int[]{1, 10},
+
+        });
+
+        log.info("max families: {}", maxFamilies);
     }
 
 }
