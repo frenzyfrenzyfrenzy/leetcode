47b7a180093f47e469addfbe3f23d5ae53844ab3
ilyasvintsov <zaz1993@gmail.com>
2024-10-06 20:56:53 +0200

    all combinatinos with restrictions

--- src/main/java/com/svintsov/cinema/Solution.java
+++ src/main/java/com/svintsov/cinema/Solution.java
@@ -4,28 +4,37 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.stream.Stream;
 
 @Slf4j
 class Solution {
 
-    public List<List<Integer>> combine(int listSize, int segmentSize) {
+    public List<List<Integer>> combine(List<Integer> rowOccupation, Set<Entry<Integer, Integer>> aisles, int familySize) {
 
         ArrayList<List<Integer>> possibleCombinations = new ArrayList<>();
-        List<Integer> defualtList = new ArrayList<>(Stream.generate(() -> 0)
-                .limit(listSize)
+
+        List<Integer> emptyRow = new ArrayList<>(Stream.generate(() -> 0)
+                .limit(rowOccupation.size())
                 .toList());
 
-        combineInternal(defualtList, segmentSize, 0, possibleCombinations);
+        combineInternal(rowOccupation, emptyRow, aisles, familySize, 0, possibleCombinations);
 
         return possibleCombinations;
     }
 
     private void combineInternal(
-            List<Integer> currentCombination, int segmentSize, int lastPosition, List<List<Integer>> possibleCombinations) {
+            List<Integer> currentOccupation,
+            List<Integer> currentCombination,
+            Set<Entry<Integer, Integer>> aisles,
+            int segmentSize,
+            int lastPosition,
+            List<List<Integer>> possibleCombinations) {
 
         int currentStartPosition;
-        if (lastPosition==0) {
+        if (lastPosition == 0) {
             currentStartPosition = 0;
         } else {
             currentStartPosition = lastPosition + 2;
@@ -36,16 +45,45 @@ class Solution {
             return;
         }
 
-        while ((currentCombination.size() - currentStartPosition) >= segmentSize) {
-            for (int j = 0; j < segmentSize; ++j) {
-                currentCombination.set(currentStartPosition + j, 1);
-            }
-            combineInternal(currentCombination, segmentSize, currentStartPosition + segmentSize - 1, possibleCombinations);
-            for (int j = 0; j < segmentSize; ++j) {
-                currentCombination.set(currentStartPosition + j, 0);
+        while (currentStartPosition < currentCombination.size() - 1) {
+            if (possibleToPutSegment(segmentSize, currentOccupation, aisles, currentCombination, currentStartPosition)) {
+                for (int j = 0; j < segmentSize; ++j) {
+                    currentCombination.set(currentStartPosition + j, 1);
+                }
+                combineInternal(currentOccupation, currentCombination, aisles, segmentSize, currentStartPosition + segmentSize - 1,
+                        possibleCombinations);
+                for (int j = 0; j < segmentSize; ++j) {
+                    currentCombination.set(currentStartPosition + j, 0);
+                }
             }
             currentStartPosition++;
         }
+    }
+
+    private boolean possibleToPutSegment(int segmentSize,
+                                         List<Integer> currentOccupation,
+                                         Set<Entry<Integer, Integer>> aisles,
+                                         List<Integer> currentCombination,
+                                         int startPosition) {
+        for (int i = 0; i < segmentSize; ++i) {
+            if (startPosition + i >= currentCombination.size()) {
+                return false;
+            }
+            if (currentOccupation.get(startPosition + i) == 1) {
+                return false;
+            }
+            if (i == 0) {
+                if (aisles.contains(Map.entry(startPosition, startPosition + 1))) {
+                    return false;
+                }
+            }
+            if (i == segmentSize - 1) {
+                if (aisles.contains(Map.entry(startPosition + segmentSize, startPosition + segmentSize - 1))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
--- src/test/java/com/svintsov/cinema/SolutionTest.java
+++ src/test/java/com/svintsov/cinema/SolutionTest.java
@@ -1,10 +1,16 @@
 package com.svintsov.cinema;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+import static java.util.Map.entry;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 
 @Slf4j
 class SolutionTest {
@@ -18,7 +24,12 @@ class SolutionTest {
 
     @Test
     public void test() {
-        List<List<Integer>> result = solution.combine(10, 3);
+        List<Integer> occupation = newArrayList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        Set<Entry<Integer, Integer>> aisles = newHashSet(entry(2,3), entry(6,7));
+
+        List<List<Integer>> result = solution.combine(occupation, aisles, 4);
+
         result.forEach(integers -> log.info("{}", integers));
     }
 
